4506b5fe291b181d44271d61af96f8354e2b49e0
Javier Luraschi <javierluraschi@hotmail.com>
2020-02-11 16:42:56 -0800

    resync headers

--- src/lantern/lantern.h
+++ src/lantern/lantern.h
@@ -136,9 +136,6 @@ bool lanternInit(const std::string& libPath, std::string* pError)
   if (!lanternLoadLibrary(libPath, pError))
     return false;
   
-  if (!lanternLoadLibrary(libPath, pError))
-    return false;
-  
   LOAD_SYMBOL(lanternTest);
   
   return true;
